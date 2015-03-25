1df2f64e335b589dc89cf5229549a03e89699428
Luis Franco <alfredo@verynicetech.com>
2015-03-24 21:31:09 -0600

    Actualizando docuemntacion, Diagrama ER. Modelo fisico de base de datos a utilizar

--- base de datos/contenido veaXML.sql
+++ base de datos/contenido veaXML.sql
@@ -2,7 +2,9 @@ insert into formato(descripcion) values('XML');
 insert into formato(descripcion) values('XSD');
 insert into formato(descripcion) values('DTD');
 
-insert into tipo(tipo) values("texto");
-insert into tipo(tipo) values("digito");
+insert into tipo(tipo) values("string");
 insert into tipo(tipo) values("decimal");
-insert into tipo(tipo) values("logico");
+insert into tipo(tipo) values("integer");
+insert into tipo(tipo) values("boolean");
+insert into tipo(tipo) values("date");
+insert into tipo(tipo) values("time");
