5f09894e16a1e405103c0cb79a26a4cb3d72eab7
Luis <ilusion.linux@gmail.com>
2015-05-07 15:37:56 -0600

    Tablas para propiedades de usaurio del programa.

---
+++ base de datos/parametros veaXML.sql
@@ -0,0 +1,13 @@
+--Propiedades
+insert into propiedad(propiedad, valor) values('ventana.maximizada', null);
+insert into propiedad(propiedad, valor) values('ventana.posicion.x', null);
+insert into propiedad(propiedad, valor) values('ventana.posicion.y', null);
+insert into propiedad(propiedad, valor) values('ventana.ancho', null);
+insert into propiedad(propiedad, valor) values('ventana.alto', null);
+insert into propiedad(propiedad, valor) values('programa.idioma', null);
+
+--Abiertos
+insert into abiertos(documento) values('');
+
+--Historial
+insert into historial(documento, accion) values(null, null);
